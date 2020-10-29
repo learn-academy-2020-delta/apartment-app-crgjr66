469773bda3deeebc2523e3a75f16b3eb2a28acad
crgjr66 <chuckgilbreth@yahoo.com>
2020-10-29 10:26:58 -0700

    added delete and update functionality, plus some styling

--- app/javascript/components/pages/MyApartmentIndex.js
+++ app/javascript/components/pages/MyApartmentIndex.js
@@ -14,9 +14,17 @@ class MyApartmentIndex extends Component{
                 <h3>{ apartment.city }</h3>
                 <h3>{ apartment.state }</h3>
                 <br />
-                <Link to={`/apartmentshow/${apartment.id}`} className="button">
-                  More Info
-                </Link>
+                <div id="my-buttons">
+                  <Link to={`/apartmentshow/${apartment.id}`} className="button">
+                    More Info
+                  </Link>
+                  <Link to={`/apartmentedit/${apartment.id}`} className="button">
+                    Edit Listing
+                  </Link>
+                  <Link onClick={ () => { this.props.deleteApartment(apartment.id) } } className="button">
+                    Remove Listing
+                  </Link>
+                </div>
               </div>
             )
           })}
